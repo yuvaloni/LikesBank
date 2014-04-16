18b0fbe58a16296cf3688f5e5e7c4b9569b3564b
yuvaloni <yuvon2@gmail.com>
2014-04-16 21:11:01 +0300

    shit

--- LikesBank/LikesBank/WebForm1.aspx.cs
+++ LikesBank/LikesBank/WebForm1.aspx.cs
@@ -18,7 +18,7 @@ namespace LikesBank
                 Response.Redirect("https://www.facebook.com/dialog/oauth?client_id=237726383082723&redirect_uri=http://likesbank.apphb.com/webform1.aspx&response_type=code");
             else
             {
-                HttpWebRequest fuck = (HttpWebRequest)WebRequest.Create(string.Format("https://graph.facebook.com/oauth/access_tokenclient_id={0}&redirect_uri={1}&client_secret={2}&code={3})", "237726383082723", "http://likesbank.apphb.com/webform1.aspx", "0420278b8f5a0985ba21458afac9e257", Request.Params["code"].ToString()));
+                HttpWebRequest fuck = (HttpWebRequest)WebRequest.Create(string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3})", "237726383082723", "http://likesbank.apphb.com/webform1.aspx", "0420278b8f5a0985ba21458afac9e257", Request.Params["code"].ToString()));
                 StreamReader g = new StreamReader(fuck.GetResponse().GetResponseStream());
                 string u = "";
                 while (u != null)
