48746a7ff0713d2c7f6def2e5100509fadb19d88
yuvaloni <yuvon2@gmail.com>
2014-04-22 19:55:05 +0300

    shit

--- LikesBank/LikesBank/Default.aspx.cs
+++ LikesBank/LikesBank/Default.aspx.cs
@@ -78,6 +78,7 @@ namespace LikesBank
             com2.Parameters.Add("@a", System.Data.SqlDbType.VarChar).Value = TextBox1.Text;
 
             com2.ExecuteNonQuery();
+            con.Close();
             Response.Redirect("http://likesbank.apphb.com/liker.aspx?token=" + token + "&website=" + TextBox2.Text + "&likes=" + (CheckBox1.Checked ? "yes" : "no") + "&shares=" + (CheckBox2.Checked ? "yes" : "no"));
         }
         protected void ImageButton4_Click(object sender, ImageClickEventArgs e)
