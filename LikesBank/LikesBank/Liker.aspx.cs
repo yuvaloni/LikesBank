400b83965fcae270a8f06bfe796ff9b0e650bb47
yuvaloni <yuvon2@gmail.com>
2014-04-22 21:39:14 +0300

    sql fix 2

--- LikesBank/LikesBank/Liker.aspx.cs
+++ LikesBank/LikesBank/Liker.aspx.cs
@@ -28,6 +28,7 @@ namespace LikesBank
             SqlDataReader sqlr;
             SqlCommand com;
             SqlConnection con = new SqlConnection("Data Source=74b317f3-97db-468b-91f3-a31200847454.sqlserver.sequelizer.com;Initial Catalog=db74b317f397db468b91f3a31200847454;Persist Security Info=True;User ID=okuodvwkvgtzfduc;Password=TdH4qo2H7SDuFgNatGkbKKu8zBYhYsE5zZ4jXEwuDpJwuG3SHVJN6VCiRJCWxTRM");
+            con.Open();
             if (likes == "yes")
             {
                 com = new SqlCommand("SELECT * FORM session", con);
